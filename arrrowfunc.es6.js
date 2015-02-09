ec335bf6ef12483c5fa89d8f14b4fa04de041140
Felipe Apostol <flipjs.io@gmail.com>
2015-02-09 16:54:00 +0000

    add generator and update arrowfunc

--- arrrowfunc.es6.js
+++ arrrowfunc.es6.js
@@ -9,25 +9,31 @@ let blockAll = () => {
 }
 
 let sum = add(4,5)
-console.log(sum)
+console.log('sum', sum)
 
 let sq = square(five())
-console.log(sq)
+console.log('square', sq)
 
-console.log(all())
-console.log(blockAll())
+console.log('all', all())
+console.log('blockAll', blockAll())
 
 let numbers = [1,2,3,4]
 sum = 0
 
 numbers.forEach(x => sum += x)
-console.log(sum) // 10
+console.log('arrowfunc inline', sum) // 10
+
+// same as above, but made the arrowfunc as block
+numbers.forEach(x => {
+	sum += x
+})
+console.log('arrowfunc block', sum) // 10
 
 sum = numbers.reduce((x, y) => x + y)
-console.log(sum) // 10
+console.log('sum-reduce', sum) // 10
 
 let doubled = numbers.map(x => x * 2)
-console.log(doubled) // [2,4,6,8]
+console.log('doubled', doubled) // [2,4,6,8]
 
 // solves the 'this' in inner functions like function within a function or
 // callbacks. lexical binding continues inside another inner function
