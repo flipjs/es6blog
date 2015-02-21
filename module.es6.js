20843b4bce7a78d11d3fcc01b8b2303b1d411ba9
Felipe Apostol <flipjs.io@gmail.com>
2015-02-21 11:47:35 +0000

    rename arrowfunc

--- module.es6.js
+++ module.es6.js
@@ -1,6 +1,10 @@
 // module keyword not supported in traceur?
-module m from './person_module.es6.js'
+// module m from './person_module.es6.js'
+//
+// let person = new m.Person('Chris')
+// person.canWalk()
+// console.log(m.awesome)
 
-let person = new m.Person('Chris')
+import {Person} from './person_module.es6.js'
+let person = new Person('Chris')
 person.canWalk()
-console.log(m.awesome)
